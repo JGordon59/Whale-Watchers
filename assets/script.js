94202dc3a356bd4eff2be0866d6fa34093c3c241
Zachary-Maddox <z19s96p@aol.com>
2022-07-07 10:02:02 -0500

    added two sections for stock news and crypto news plus APIs

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+fetch(
+    "https://content.guardianapis.com/search?q=Stocks&api-key=88698a0f-0636-4f4c-8bc0-f3820ad600f5"
+)
+    .then((response) => {
+        console.log(response);
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        document.getElementById("stockNews");
+    });
+
+fetch("https://finnhub.io/api/v1/add?token=cb3evlaad3i8tak0u1ug")
+    .then((response) => {
+        console.log(response);
+        return response.json();
+    })
+    .then((data) => {
+        console.log(data);
+        document.getElementById("cryptoNews");
+    });
