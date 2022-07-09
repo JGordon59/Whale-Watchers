ccd0a329fbd1046d74586d62ca0ce748b071a1fb
VictorCast01 <victorcastellon01@gmail.com>
2022-07-09 16:30:20 -0400

    small changes in order to pull new update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function myFunction() {
    var element = document.body;
    element.classList.toggle("dark-mode");
-
+}
 
 function whaleBtn() {
    const funFactEl = document.getElementById('funFact');
