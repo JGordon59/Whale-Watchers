7b81ab3a29a3a129f5da619999ae7aecbfcaf695
VictorCast01 <victorcastellon01@gmail.com>
2022-07-12 13:22:34 -0400

    funfact change

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
-    function whaleBtn() {
-        const funFactEl = document.getElementById("funFact");
-        if (funFactEl.style.visibility === "hidden") {
-            funFactEl.style.visibility = "visible";
-        } else {
-            funFactEl.style.visibility = "hidden";
-        }
+function whaleBtn() {
+    const funFactEl = document.getElementById("funFact");
+    if (funFactEl.classList.contains("display-none")) {
+        funFactEl.classList.remove("display-none");
+        funFactEl.classList.add("animate-character");
+    } else {
+        funFactEl.classList.remove("animate-character");
+        funFactEl.classList.add("display-none");
     }
+}
 
 
